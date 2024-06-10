48748afe20757e443b09d90e9b57fb616022ea10
enoatu <acro1452@gmail.com>
2024-06-10 15:57:56 +0900

    modify date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -11,7 +11,7 @@ export const fmtDate = (date: Date | undefined | null) => {
   if (!date) {
     return null;
   }
-  let result = `${date.getMonth().toString().padStart(2, "0")}/${date.getDate().toString().padStart(2, "0")}`;
+  let result = `${(date.getMonth() + 1).toString().padStart(2, "0")}/${date.getDate().toString().padStart(2, "0")}`;
   const dayOfWeek = ["日", "月", "火", "水", "木", "金", "土"][date.getDay()];
   result += `(${dayOfWeek})`;
   return result;
